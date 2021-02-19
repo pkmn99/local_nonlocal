c87639106f578b5e644af0b76aba63317295b13b
Thinkpad T480 <pkmn99@gmail.com>
2021-02-19 11:23:17 +0800

    fixed the constant value bug

--- code/save_clm_subgrid_2d.py
+++ code/save_clm_subgrid_2d.py
@@ -8,7 +8,7 @@ def convert_to_2d(df, var):
         for i in range(1,17):
             temp = np.full_like(df.landmask, np.nan)
             ind = df.pfts1d_itype_veg==i
-            temp[df.pfts1d_jxy[ind]-1, df.pfts1d_ixy[ind]-1] = df[var][0,ind]
+            temp[df.pfts1d_jxy[ind]-1, df.pfts1d_ixy[ind]-1] = df[var][t,ind]
             a[t,i,:,:]=temp
     return  xr.DataArray(a, 
                      coords=[df.time, np.int32(list(range(0,17))), df.lat, df.lon],
@@ -20,13 +20,16 @@ def combine_dataframe(df, var_list=['EFLX_LH_TOT', 'FGR', 'FIRE', 'FSA', 'FSH', 
     return  xr.merge(da_all)
 
 def save_to_2d(fn_in, fn_out):
-    dc2=xr.open_dataset(fn_in, decode_times=False)
+    dc2=xr.open_dataset(fn_in)
+    dc2.load()
     df_final = combine_dataframe(dc2)
     df_final.to_netcdf(fn_out)
     print('file saved')
 
 if __name__=="__main__":
-    exp='F2000climo_ctl'
-    fn_in='../data/outputdata/%s.clm2.h1.0001-0035-yearmean.nc'%exp
-    fn_out='../data/outputdata/%s.clm2.h1.0001-0035-yearmean-2d.nc'%exp
+#    exp='F2000climo_ctl'
+    exp='F2000climo_Allgrass_minimal_tree'
+    time_scale='monthly'
+    fn_in='../data/outputdata/%s.clm2.h1.0001-0035-%s.nc'%(exp,time_scale)
+    fn_out='../data/outputdata/%s.clm2.h1.0001-0035-%s-2d.nc'%(exp,time_scale)
     save_to_2d(fn_in, fn_out)
