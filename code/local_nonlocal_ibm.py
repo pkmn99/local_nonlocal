428a12587d4368034744aadae1ac60903eb2beed
Yan_dellwork <pkmn99@gmail.com>
2024-07-02 15:14:40 +0800

    add some code comment

--- code/local_nonlocal_ibm.py
+++ code/local_nonlocal_ibm.py
@@ -37,12 +37,15 @@ def save_data():
     swout_c=c_clm['FSR'] # reflected shortwave radiation
     swout_g=g_clm['FSR'] # reflected shortwave radiation
     
+    # calculate surface temperature from longwave radiation
     ts_c=(lwout_c/(1*B))**0.25
     ts_g=(lwout_g/(1*B))**0.25
     
+    # calculate abledo
     alb_c=(c_clm.FSR/c_clm.FSDS)
     alb_g=(g_clm.FSR/g_clm.FSDS)
-    
+
+    # atmoshperic air temperature as Ta
     tair_c=c_cam['T'][:,31] # air temperature of the bottom atmosphere level
     tair_g=g_cam['T'][:,31] # air temperature of the bottom atmosphere level
     
@@ -54,19 +57,21 @@ def save_data():
     ts_diff = ts_g-ts_c # actual surface temperature difference
     
     # Calculate necessary variables for IBM method
-    ra_c =(ts_c-tair_c)/h_c * 1.2250 * 1003.5
-    bo_c = h_c/le_c
-    lam_c = 1/(4*B*tair_c**3)
-    F_c = (1.2250 * 1003.5 *lam_c)/ra_c*(1+1/bo_c)
+    ra_c =(ts_c-tair_c)/h_c * 1.2250 * 1003.5 # aerodynamic roughness
+    bo_c = h_c/le_c # bowen ratio
+    lam_c = 1/(4*B*tair_c**3) #lambda term
+    F_c = (1.2250 * 1003.5 *lam_c)/ra_c*(1+1/bo_c) # energy redistribution factor
     
     ra_g =(ts_g-tair_g)/h_g * 1.2250 * 1003.5
     bo_g = h_g/le_g
     lam_g = 1/(4*B*tair_g**3)
     F_g = (1.2250 * 1003.5 *lam_g)*(1+1/bo_g)/ra_g
     
+    # Net radiation
     Rn_c = swin_c - swout_c + lwin_c - 1*B*tair_c**4
     Rn_g = swin_g - swout_g + lwin_g - 1*B*tair_g**4
     
+    # Use CTL to calculate IBM
     ra = ra_c
     bo = bo_c
     F = F_c
@@ -75,17 +80,18 @@ def save_data():
     delta_f1= -lam_c *1.2250*1003.5 *(1+1/bo) * (ra_g-ra_c)/ra**2
     delta_f2= -lam_c *1.2250*1003.5 * (bo_g-bo_c)/(ra*bo**2)
     
+    # Decomposed tempreature
     dts1 = lam_c /(1+F) * (c_clm.FSDS*-(alb_g-alb_c))
     
     dts2 = -lam_c/((1+F)**2) * Rn * delta_f1
     
     dts3 = -lam_c/(1+F)**2 * Rn * delta_f2
     
-    dts4=tair_g-tair_c
+    dts4=tair_g-tair_c # changes in backgroud cliamte by Ta 
     
     dts=dts1+dts2+dts3
-    
-    local=dts.where((dts>ts_diff.quantile(0.001))&(dts<ts_diff.quantile(0.999))).mean(dim='time')
+
+    local=dts.where((dts>ts_diff.quantile(0.001))&(dts<ts_diff.quantile(0.999))).mean(dim='time') # remove outliers
     nonlocal_tair=dts4.mean(dim='time')
     nonlocal_tsc=ts_diff.mean(dim='time')-local
     
